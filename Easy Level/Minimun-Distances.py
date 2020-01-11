a5796cec70f90f4c0944a987ae40600a63ed401e
Suryakant Kumar <skantgo@gmail.com>
2020-01-11 12:59:02 +0530

    Minimum Distances

--- Easy Level/Minimun-Distances.py
+++ Easy Level/Minimun-Distances.py
@@ -37,13 +37,8 @@ def minimumDistances(a):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input())
-
     a = list(map(int, input().rstrip().split()))
-
     result = minimumDistances(a)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
